909f83f78a14958fe0e13ac1da9aa8ec6a27f1fb
Maryam Nasir <maryamnasir555@yahoo.com>
2024-09-14 17:36:13 +0500

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "hsl(1, 90%, 64%)",
+        blue: "hsl(219, 85%, 26%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrayishBlue: {
+          DEFAULT: "hsl(210, 60%, 98%)",
+          1: "hsl(211, 68%, 94%)",
+          2: "hsl(205, 33%, 90%)",
+        },
+        grayishBlue: "hsl(219, 14%, 63%)",
+        darkGrayishBlue: "hsl(219, 12%, 42%)",
+        darkBlue: "hsl(224, 21%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
